fca77914ddbcf67b6aac5b8c7064ad9a6f43f292
jackfperryjr <jackfperryjr@gmail.com>
2021-10-03 14:21:36 +0000

    gh-pages

---
+++ precache-manifest.f21618160d0df28ca87083baac6597c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15df99b858304445d2aa30260e21da7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "486283e389b58a807fce",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3484fe9939dc7baab979",
+    "url": "/static/css/main.d1558497.chunk.css"
+  },
+  {
+    "revision": "486283e389b58a807fce",
+    "url": "/static/js/2.c04a1558.chunk.js"
+  },
+  {
+    "revision": "607ccb8e5cfe57a5e67c7962c2feae47",
+    "url": "/static/js/2.c04a1558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3484fe9939dc7baab979",
+    "url": "/static/js/main.ec56f00c.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
